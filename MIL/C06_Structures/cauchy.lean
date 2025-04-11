79c1b5b7278db7ad4e3567542f1b341dd1e12b42
bwehlin <bwehlin@kth.se>
2025-04-11 23:04:53 +0200

    wip

--- MIL/C06_Structures/cauchy.lean
+++ MIL/C06_Structures/cauchy.lean
@@ -2,6 +2,8 @@ import MIL.Common
 import Mathlib.Algebra.Group.TypeTags.Basic
 import Mathlib.Data.Finite.Defs
 import Mathlib.Data.Fintype.Card
+import Init.Data.Vector.Basic
+import Mathlib.Logic.Equiv.Array
 
 #check Group
 
@@ -67,3 +69,140 @@ theorem Cauchy₁ {G : Type*} [Fintype G] [Group G] (p : ℕ) (hp : Nat.Prime p)
   --intro pdiv_card
   --constructor
   --sorry
+
+theorem test123 (p : ℕ) (hp: Nat.Prime p) :
+  p ≥ 0 := by
+
+  let X := {x : Vector ℕ p | p ≥ 0}
+
+  have : NeZero p := by
+    constructor
+
+    apply Nat.Prime.ne_zero at hp -- gives hp : p ≠ 0
+    --exact hp doesn't work here, for example (neither does simp, linarith)
+    sorry
+
+  have : ∀ x ∈ X, x.head ≥ 0 := by
+    sorry
+
+
+theorem test1231 (p : ℕ) (hp: Nat.Prime p) :
+  p ≥ 0 := by
+
+  let X := {x : Vector ℕ p | p ≥ 0}
+
+  have : ∀ x ∈ X, x.head ≥ 0 := by
+    sorry
+
+lemma fold_identa {G: Type*} [Group G] {p : ℕ} [NeZero p] (hp : p ≥ 1) (x : List G) (g : G) (f : G → G → G) :
+  f (x.dropLast.foldl f 1) g = x.foldl f 1 := by
+
+  have : x = x.dropLast ++ [g] := by sorry
+  nth_rw 2 [this]
+  rw[List.foldl_concat f 1 g x.dropLast]
+
+lemma fold_identb {G: Type*} [Group G] {p : ℕ} [NeZero p] (hp : p ≥ 1) (x : Vector G p) (g : G) (f : G → G → G) :
+  f (x.pop.foldl f 1) x.back = x.foldl f 1 := by
+
+  let xl := x.toList
+  let xlm := x.pop.toList
+
+  --have : xl = xl.dropLast ++ [xl.getLast] := by
+  --  symm
+  --  apply List.dropLast_append_getLast
+
+  have : xl ≠ [] := by sorry
+  have lem : x.back = xl.getLast this := by
+    sorry
+
+  have : xl = xl.dropLast ++ [x.back] := by
+    rw [lem]
+    rw [List.dropLast_append_getLast this]
+
+
+  rw [← List.dropLast_append_getLast]
+  rw[List.foldl_concat f 1 g x.dropLast]
+
+lemma fold_ident {G: Type*} [Group G] {p : ℕ} [NeZero p] (hp : p ≥ 1) (x : Vector G p) (f : G → G → G) :
+  f (x.pop.foldl f 1) x.back = x.foldl f 1 := by
+
+  --rw [← List.head_eq_getElem_zero]
+  --simp
+
+  have : ∃ m : ℕ, p = m + 1 := by
+    cases p with
+    | zero => contradiction
+    | succ n => simp
+
+  rcases this with ⟨m, hpm⟩
+  rw [List.foldl_concat]
+
+
+
+  sorry
+
+lemma fold_ident {G: Type*} [Group G] {p : ℕ} [NeZero p] (hp : p ≥ 1) (x : Vector G p) (f : G → G → G) :
+  f x.head (x.tail.foldl f 1) = x.foldl f 1 := by
+  simp
+  rw [Equiv.arrayEquivList]
+  rw [← List.cons_head?_tail]
+
+
+lemma fold_ident {G: Type*} [Group G] {p : ℕ} [NeZero p] (hp : p ≥ 1) (x : Vector G p) :
+  x.head * x.tail.foldl Mul.mul 1 = x.foldl Mul.mul 1 := by
+  --simp
+
+
+  cases p
+
+  contradiction
+
+
+
+
+  induction' p with n hn
+  contradiction
+
+
+
+
+
+
+
+
+  sorry
+
+
+theorem Cauchy₃ {G: Type*} [Fintype G] [Group G] (p : ℕ) (hp : Nat.Prime p) (pdvd : p ∣ Fintype.card G) :
+  ∃ x : G, orderOf x = p := by
+
+  let X := { x : Vector G p | x.foldl Mul.mul 1 = 1  }
+
+  have : NeZero p := by
+    -- Got some help here: https://leanprover.zulipchat.com/#narrow/channel/270676-lean4/topic/Missing.20something.20fundamental.20about.20Props/with/511666602
+    apply Nat.Prime.ne_zero at hp
+    apply neZero_iff.mpr at hp
+    exact hp
+
+  have : ∀ x ∈ X, x.head * x.tail.foldl Mul.mul 1 = x.foldl Mul.mul 1 := by
+    rintro x xmem
+    simp
+
+
+
+  have : ∀ x ∈ X, x.tail.foldl Mul.mul 1 = x.head⁻¹ := by
+    rintro x xmem
+    rw [← one_mul (x.tail.foldl Mul.mul 1)]
+    nth_rw 1 [← inv_mul_cancel x.head]
+    rw [mul_assoc, ← inv_inv (x.head * x.tail.foldl Mul.mul 1), mul_inv_eq_iff_eq_mul]
+
+
+
+    symm
+    rw[← div_eq_iff_eq_mul, div_eq_mul_inv, inv_inv]
+    rw [← one_mul (x.tail.foldr Mul.mul 1)⁻¹, mul_assoc]
+
+    nth_rw 1 [← inv_mul_cancel x.head]
+
+
+    apply mul_eq_of_eq_mul_inv x.head 1 ((x.tail.foldr Mul.mul 1)⁻¹)

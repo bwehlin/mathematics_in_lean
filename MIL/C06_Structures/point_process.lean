36e498275fc25ac4abcf9560d1531f2f8ad99927
Bjorn Wehlin <bjornw@Bjorns-MacBook-Pro.local>
2025-04-16 11:32:59 +0200

    pp init

---
+++ MIL/C06_Structures/point_process.lean
@@ -0,0 +1,33 @@
+import Mathlib.Tactic
+import Mathlib.Util.Delaborators
+
+import Mathlib.MeasureTheory.Measure.MeasureSpaceDef
+import Mathlib.MeasureTheory.Measure.Dirac
+
+import Mathlib.Algebra.Group.Indicator
+
+set_option warningAsError false
+
+noncomputable section
+
+open MeasureTheory
+
+variable {α β δ : Type*} [MeasurableSpace α] [MeasurableSpace β] {s : Set α} {a : α}
+
+-- sample space α
+
+--def RandomMeasure : α → β :=
+
+
+def PointMeasure (xs : List α) : Measure α where
+    measureOf s := (xs.map (s.indicator fun _ ↦ 1)).sum
+    empty := by simp
+    mono := by
+        intro s₁ s₂ hsub
+        have : ∀ x ∈ xs, (s₁.indicator fun _ ↦ 1) x ≤ (s₂.indicator fun _ ↦ 1) x := by
+            intro x xmem
+
+
+    iUnion_nat := sorry
+    m_iUnion := sorry
+    trim_le := sorry

85847587b9bc731a0adc9ff93950797cee3a08df
bwehlin <bwehlin@kth.se>
2025-04-30 14:00:00 +0200

    one case for injective

--- MIL/C06_Structures/point_process.lean
+++ MIL/C06_Structures/point_process.lean
@@ -53,6 +53,14 @@ theorem dirac_of_singleton_eq_one_if_ne_zero {a x : α} (hm : MeasurableSet (Set
     rw [Measure.dirac_apply' a hm] at h
     exact mem_of_indicator_ne_zero h
 
+theorem split_sum {S : Set ℕ} (f : S → ENNReal) (j : S) (hgt : ∀ i, f i ≥ 0) : --(hconv : HasSum (∑' (i : S), f i)):
+    ∑' (i : S), f i = ∑' (i : {k : S | k < j}), f i + f j + ∑' (i : {k : S | k > j}), f i := by
+    refine HasSum.tsum_eq ?_
+
+theorem sum_zeroes {S : Set ℕ} (f : S → ENNReal) (hf : ∀ i, f i = 0) : ∑' (i : S), f i = 0 := by
+    exact ENNReal.tsum_eq_zero.mpr hf
+
+
 theorem is_simple_if_injective {S : Set ℕ} {f : S → α} (hf: Injective f) (hm : ∀ x : α, MeasurableSet (Set.singleton x)) : IsSimplePointMeasure f := by
     intro x
 
@@ -62,50 +70,50 @@ theorem is_simple_if_injective {S : Set ℕ} {f : S → α} (hf: Injective f) (h
         exact Set.subsingleton_of_subset_singleton fun ⦃a⦄ ↦ congrArg fun ⦃a⦄ ↦ a
         apply hf
 
-    #check S
-    #check ↑S
     by_cases hx : x ∈ (f '' univ)
 
+    ·   have : ∃ i, f i = x := by
+            refine SetCoe.exists.mpr ?_
+            simp at hx
+            exact hx
 
+        rcases this with ⟨i, hfi⟩
 
-    have : ∃ i, f i = x := by
-        refine SetCoe.exists.mpr ?_
-        simp at hx
-        exact hx
+        have : ∀ j, ∀ (h: f j = x), j = i := by
+            rintro ⟨j, hjs⟩
+            exact fun h ↦ hf (congrArg f (this h hfi))
 
-    rcases this with ⟨i, hfi⟩
+        simp [PointMeasure]
+        right
 
-    have : ∀ j, ∀ (h: f j = x), j = i := by
-        rintro ⟨j, hjs⟩
-        exact fun h ↦ hf (congrArg f (this h hfi))
-
-    simp [PointMeasure]
-    right
-
-    have : ∀ j, ∀ (h: j ≠ i), (dirac (f j)) (Set.singleton x) = (0 : ENNReal) := by
-        intro j hj
-        contrapose! hj
-        apply dirac_of_singleton_eq_one_if_ne_zero at hj
-        have : (dirac (f i)) (Set.singleton x) = (1 : ENNReal) := by
-            rw[hfi]
-            apply dirac_on_singleton_of_singleton_eq_one
+        have j_ne_i_eq_zero : ∀ j, ∀ (h : j ≠ i), dirac (f j) (Set.singleton x) = 0 := by
+            intro j
+            contrapose!
+            intro hj
+            apply dirac_of_singleton_eq_one_if_ne_zero at hj
+            rw [dirac_on_singleton_iff, ← hfi] at hj
+            exact hf hj
+            apply hm
             apply hm
 
+        have : (sum fun i ↦ dirac (f i)) (Set.singleton x) = (dirac (f i)) (Set.singleton x) := by
+            rw [MeasureTheory.Measure.sum_apply]
 
-    have : dirac (f i) (Set.singleton x) = 1 := by
+            rw[ENNReal.tsum_eq_add_tsum_ite i]
+            rw[ENNReal.tsum_eq_zero.mpr, add_zero]
+
+            intro j
+            simp [j_ne_i_eq_zero]
+            specialize j_ne_i_eq_zero j
+            apply j_ne_i_eq_zero
+            apply hm
+
+        rw[this]
         rw[hfi]
         exact dirac_apply_of_mem rfl
 
-    have : ∀ j, ∀ (h : j ≠ i), dirac (f j) (Set.singleton x) = 0 := by
-        intro j
-        contrapose!
-        intro hj
-        apply dirac_of_singleton_eq_one_if_ne_zero at hj
-
-
-
-
-    contrapose! hf
+    ·   left
+        sorry
 
     have : PointMeasure f (Set.singleton x) ≥ 2 := by
         rcases hf with ⟨hz,ho⟩
